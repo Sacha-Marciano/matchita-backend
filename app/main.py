173c65336e1c1e221035bf69a280217093de6bd7
Sacha-Marciano <Momarciano26@gmail.com>
2025-05-26 20:24:24 +0300

    delay model call

--- app/main.py
+++ app/main.py
@@ -6,11 +6,11 @@ from google.oauth2 import service_account
 
 # Load credentials from a JSON file (ensure it's in your Render environment)
 credentials = service_account.Credentials.from_service_account_file("drive-viewer-app-460607-cf8dc1b9a8e0.json")
-
-app = FastAPI()
-
 # Init VertexAI
 vertexai.init(project="571768511871", location="us-central1",credentials=credentials)
+
+
+app = FastAPI()
 
 app.include_router(duplicate_check.router)
 app.include_router(classify.router)
--- app/services/vertex_embedding.py
+++ app/services/vertex_embedding.py
@@ -1,8 +1,15 @@
 # services/vertex_embeddings.py
 from vertexai.language_models import TextEmbeddingModel
 
+model = None
 # You must init VertexAI in your main before calling this
 model = TextEmbeddingModel.from_pretrained("gemini-embedding-001")
+
+def get_embedding_model():
+    global model
+    if model is None:
+        model = TextEmbeddingModel.from_pretrained("gemini-embedding-001")
+    return model
 
 def chunk_text(text: str, max_chunk_chars: int = 1000):
     # Naive line-based chunker for now (improve later)
@@ -22,5 +29,7 @@ def embed_text_chunks(text: str):
     chunks = chunk_text(text)
     # Remove empty or whitespace-only chunks
     clean_chunks = [chunk for chunk in chunks if chunk.strip()]
+    # Get the embedding model
+    model = get_embedding_model()
     embeddings = [model.get_embeddings([chunk])[0].values for chunk in clean_chunks]
     return embeddings
