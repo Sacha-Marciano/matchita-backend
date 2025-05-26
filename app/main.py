ab3eee575c14850a96e9cf7f5439447080e9cf52
Sacha-Marciano <Momarciano26@gmail.com>
2025-05-26 17:53:37 +0300

    add google creds

--- app/main.py
+++ app/main.py
@@ -2,11 +2,15 @@
 from fastapi import FastAPI
 from app.routes import duplicate_check, classify, save_vector
 import vertexai
+from google.oauth2 import service_account
+
+# Load credentials from a JSON file (ensure it's in your Render environment)
+credentials = service_account.Credentials.from_service_account_file("drive-viewer-app-460607-cf8dc1b9a8e0.json")
 
 app = FastAPI()
 
 # Init VertexAI
-vertexai.init(project="571768511871", location="us-central1")
+vertexai.init(project="571768511871", location="us-central1",credentials=credentials)
 
 app.include_router(duplicate_check.router)
 app.include_router(classify.router)
