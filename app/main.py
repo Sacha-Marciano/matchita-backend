5a8dfa4b1d35b89bc377affe77e0921af3fd731d
Sacha-Marciano <Momarciano26@gmail.com>
2025-05-26 20:36:39 +0300

    use .env for creds

--- app/main.py
+++ app/main.py
@@ -2,10 +2,17 @@
 from fastapi import FastAPI
 from app.routes import duplicate_check, classify, save_vector
 import vertexai
+from dotenv import load_dotenv
 from google.oauth2 import service_account
+import os
+import json
+
+load_dotenv()
 
 # Load credentials from a JSON file (ensure it's in your Render environment)
-credentials = service_account.Credentials.from_service_account_file("drive-viewer-app-460607-cf8dc1b9a8e0.json")
+SERVICE_ACCOUNT_INFO = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"])
+credentials = service_account.Credentials.from_service_account_info(SERVICE_ACCOUNT_INFO)
+
 # Init VertexAI
 vertexai.init(project="571768511871", location="us-central1",credentials=credentials)
 
