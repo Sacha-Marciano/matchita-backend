c344da2ef700ec73b039c9590dcc41ad7411aec5
Sacha-Marciano <Momarciano26@gmail.com>
2025-05-27 15:44:51 +0300

    revert changes

--- app/main.py
+++ app/main.py
@@ -1,25 +1,11 @@
-# main.py
 from fastapi import FastAPI
 from app.routes import duplicate_check, classify, save_vector
 import vertexai
 
-import os
-import json
-from google.oauth2 import service_account
-
-# Load credentials from a JSON file (ensure it's in your Render environment)
-credentials_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
-if not credentials_json:
-    raise ValueError("GOOGLE_APPLICATION_CREDENTIALS_JSON not set")
-
-credentials_dict = json.loads(credentials_json)
-credentials = service_account.Credentials.from_service_account_info(credentials_dict)
+app = FastAPI()
 
 # Init VertexAI
-vertexai.init(project="571768511871", location="us-central1",credentials=credentials)
-
-
-app = FastAPI()
+vertexai.init(project="571768511871", location="us-central1")
 
 app.include_router(duplicate_check.router)
 app.include_router(classify.router)
